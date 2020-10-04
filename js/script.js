9de29fa5ebd0d91996a0b74c7e94103e69d3d154
tsotne <mail_to_tsotne@yahoo.com>
2020-10-04 21:56:03 +0400

    loop through colorSquare

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ document.getElementById("addSquare").addEventListener("click", addSquare);
 document.getElementById("addCircle").addEventListener("click", addCircle);
 
 let colorSquare =  document.getElementsByClassName("colorSquare");
-let selectedSquare = document.getElementsByClassName("selected")[0].style.backgroundColor;
+let selectedSquare = document.getElementsByClassName("selected")[0];
 
 
 for (let i=0; i<colorSquare.length; i++){
 	function copyColor () {
 		let cssprop = window.getComputedStyle(colorSquare[i], null).getPropertyValue("background-color");
-		selectedSquare = cssprop;
+		selectedSquare.style.backgroundColor = cssprop;
 	}
 	colorSquare[i].addEventListener("click", copyColor);
 }
