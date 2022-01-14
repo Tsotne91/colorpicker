00422f5a5d52335328f6f4bfd06d1da4eaadf8ac
Tsotne91 <tsotne91@gmail.com>
2022-01-14 21:35:38 +0400

    Final version

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function addFigure (shape){
 	let newFigure = document.createElement("div");
 	newFigure.classList.add(shape);
 	document.getElementById("container").appendChild(newFigure);
+	newFigure.addEventListener("click", applyColor);
 }
 function addSquare (){
 	addFigure("createdSquare");
@@ -15,7 +16,6 @@ document.getElementById("addCircle").addEventListener("click", addCircle);
 let colorSquare =  document.getElementsByClassName("colorSquare");
 let selectedSquare = document.getElementsByClassName("selected")[0];
 
-
 function copyColor (e) {
 	selectedSquare.style.backgroundColor = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
 }
@@ -23,15 +23,7 @@ for (let i=0; i<colorSquare.length; i++){
 	colorSquare[i].addEventListener("click", copyColor);
 }
 
-let createdDivs = document.getElementById("container").children;
-
 function applyColor (event){
 	event.target.style.backgroundColor = selectedSquare.style.backgroundColor;
 	console.log(event.target);
 }
-
-	for (let div of createdDivs){
-		div.addEventListener("click", applyColor);
-
-	}
-
