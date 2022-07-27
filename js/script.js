97d5b6138a1a80131decc59b1c57b539a1d39cb9
Tsotne91 <tsotne91@gmail.com>
2022-07-27 23:36:23 +0400

    Optimized

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
-function addFigure (shape){
-	let newFigure = document.createElement("div");
-	newFigure.classList.add(shape);
-	document.getElementById("container").appendChild(newFigure);
-	newFigure.addEventListener("click", applyColor);
+let counter = 0;
+const addButtons = document.getElementsByClassName("add");
+const colorSquares = document.getElementsByClassName("colorSquare");
+const selectedSquare = document.getElementsByClassName("selected")[0];
+
+const addFigure = (shape) => {
+    if (counter < 49) {
+        let newFigure = document.createElement("div");
+        newFigure.classList.add(shape);
+        document.getElementById("container").appendChild(newFigure);
+        newFigure.addEventListener("click",
+                event => event.target.style.backgroundColor = selectedSquare.style.backgroundColor
+        )
+    }
+	else {
+		for (const button of addButtons){
+			button.style.disabled = true;
+            button.style.backgroundColor = "grey";
+		}
+		alert("no more shapes can be added");
+	}
 }
-function addSquare (){
-	addFigure("createdSquare");
+
+function addSquare() {
+    counter++;
+    addFigure("createdSquare");
+    console.log(counter);
 }
-function addCircle(){
-	addFigure("createdCircle");
+
+function addCircle() {
+    counter++;
+    addFigure("createdCircle");
+    console.log(counter);
 }
+
 document.getElementById("addSquare").addEventListener("click", addSquare);
 document.getElementById("addCircle").addEventListener("click", addCircle);
 
-let colorSquare =  document.getElementsByClassName("colorSquare");
-let selectedSquare = document.getElementsByClassName("selected")[0];
-
-function copyColor (e) {
-	selectedSquare.style.backgroundColor = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
-}
-for (let i=0; i<colorSquare.length; i++){
-	colorSquare[i].addEventListener("click", copyColor);
+function copyColor(e) {
+    selectedSquare.style.backgroundColor = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
 }
 
-function applyColor (event){
-	event.target.style.backgroundColor = selectedSquare.style.backgroundColor;
-	console.log(event.target);
+for (const colorSquare of colorSquares){
+    colorSquare.addEventListener("click", copyColor);
 }
+
